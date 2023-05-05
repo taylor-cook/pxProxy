49035c120a938083f62b7c27113fa5b6d5e63503
taylor-cook <bosstaylor@gmail.com>
2023-05-05 16:52:11 -0400

    API test success

--- pxproxy/api.py
+++ pxproxy/api.py
@@ -1,26 +1,16 @@
-import requests
 from flask import Flask, render_template
-from flask-socketio import SocketIO, emit
+import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
-# route for the home page
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# function to get data from JSONPlaceholder API
-def get_data():
+    # Make API request
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    return response.json()
+    data = response.json()
 
-# socket.io event to emit data to the client
-@socketio.on('connect')
-def test_connect():
-    data = get_data()
-    emit('data', data)
+    # Render template with API data
+    return render_template('index-api.html', posts=data)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(host='10.0.1.240',debug=True)
