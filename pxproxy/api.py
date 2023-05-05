8b2d595c5581dee38df02135422231e25c986d9c
taylor-cook <bosstaylor@gmail.com>
2023-05-05 16:08:51 -0400

    API testing

---
+++ pxproxy/api.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# function to get data from JSONPlaceholder API
+def get_data():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    return response.json()
+
+# socket.io event to emit data to the client
+@socketio.on('connect')
+def test_connect():
+    data = get_data()
+    emit('data', data)
+
+if __name__ == '__main__':
+    socketio.run(app)
